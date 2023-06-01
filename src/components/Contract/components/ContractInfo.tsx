177590527bae8d3f787dd6d5485ceca485464216
adonai_cn@hotmail.com <adonai_cn@hotmail.com>
2023-06-01 08:23:23 -0300

    FEAT: show total amount due shipowner

--- src/components/Contract/components/ContractInfo.tsx
+++ src/components/Contract/components/ContractInfo.tsx
@@ -1,3 +1,4 @@
+import BigNumber from 'bignumber.js';
 import { ContractStatus } from "@/interfaces/ContractStatus.interface";
 import { Text } from "@chakra-ui/react"
 
@@ -7,12 +8,17 @@ interface Props {
 }
 
 export default function ContractInfo({ contractAddress, contractStatus }: Props) {
+    const bigNumber = new BigNumber(contractStatus.totalAmountDueToPay);
+    const floatValue = bigNumber.toNumber();
+
     return (
         <>
             <Text as='b' style={{margin: 0}}>Contract Address:</Text>
             <Text style={{margin: 0}}>{contractAddress}</Text>
             <Text as='b' style={{margin: 0, marginTop: 4}}>Vessel IMO number:</Text>
             <Text style={{margin: 0}} textAlign="center">{contractStatus.IMOnumber}</Text>
+            <Text as='b' style={{margin: 0, marginTop: 4}}>Total amount due shipowner:</Text>
+            <Text style={{margin: 0}} textAlign="center">{floatValue.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</Text>
         </>
     )
 }
