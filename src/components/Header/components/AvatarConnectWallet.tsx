4d34be492fad28401d35e405a46c6930b790609d
adonai_cn@hotmail.com <adonai_cn@hotmail.com>
2023-06-03 09:24:20 -0300

    FEAT: only mumbai allowed

--- src/components/Header/components/AvatarConnectWallet.tsx
+++ src/components/Header/components/AvatarConnectWallet.tsx
@@ -1,23 +1,29 @@
 import { Avatar, HStack } from "@chakra-ui/react";
-import ButtonHeader from "./ButtonHeader";
-import connectWallet from "../../../../utils/connectWallet";
 import { useRouter } from 'next/router';
+import ButtonHeader from "./ButtonHeader";
+import connectWallet from "@/utils/connectWallet";
+import checkNetwork from "@/utils/checkNetwork"
 
 export default function AvatarConnectWallet({ avatarName, onOpen }:{avatarName: string, onOpen: () => void}) {
     const router = useRouter();
     
     const handleConnectButton = async() => {
-        const responseConnection = await connectWallet()
-        if (typeof responseConnection === 'string') {
-            if (responseConnection === "registeredUser") {
-                router.push('/dashboard');
-            }
-            if (responseConnection === "choseRole") {
-                router.push('/chooserole');
+        const isOnMumbai = await checkNetwork().catch(error => {
+            console.error('Ocorreu um erro:', error);
+        });
+        
+        if (isOnMumbai) {
+            const responseConnection = await connectWallet()
+            if (typeof responseConnection === 'string') {
+                if (responseConnection === "registeredUser") {
+                    router.push('/dashboard');
+                }
+                if (responseConnection === "choseRole") {
+                    router.push('/chooserole');
+                }
             }
         }
-    }
-    
+    }    
     
     return (
         <HStack>
---
+++ utils/checkNetwork.ts
@@ -0,0 +1,19 @@
+import { ethers } from 'ethers';
+
+async function checkNetwork(): Promise<boolean> {
+  if (typeof window.ethereum === 'undefined') {
+    throw new Error('MetaMask is not installed or not available.');
+  }
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const network = await provider.getNetwork();
+
+  if (network.name === 'maticmum') {
+    return true;
+  } else {
+    alert('The wallet is not connected to the Mumbai Testnet network of Polygon. You need to be connected to this network to proceed with the wallet connection.');
+    return false;
+  }
+}
+
+export default checkNetwork;
