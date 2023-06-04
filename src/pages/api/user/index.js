a6b159965f5d9fda8613a9fb52b9145050a7fdae
adonai_cn@hotmail.com <adonai_cn@hotmail.com>
2023-06-04 08:43:57 -0300

    BUILD: issue connection mongodb atlas

--- src/pages/api/user/index.js
+++ src/pages/api/user/index.js
@@ -1,5 +1,21 @@
 import User from "../../../../models/User"
-import dbConnection from "../../../../services/dbConnection"
+import mongoose from "mongoose";
+const connection = {}
+// import dbConnection from "../../../../services/dbConnection"
+
+async function dbConnection() {
+    if (connection.isConnected) return
+
+    mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+    })
+    .then(() => {
+        console.log('Connection established with MongoDB');
+      })
+      .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+      });
+}
 
 dbConnection();
 
